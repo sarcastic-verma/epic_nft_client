71e86d39365e5be771e8321cfc81e297082e5912
bruhmac <shivamthegreat.sv@gmail.com>
2022-03-27 01:40:31 +0530

    feat: added web3 related code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import twitterLogo from './assets/twitter-logo.svg';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+// Constants
+const TWITTER_HANDLE = '_buildspace';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const OPENSEA_LINK = '';
+const TOTAL_MINT_COUNT = 50;
+
+const App = () => {
+  const renderNotConnectedContainer = () => (
+      <button className="cta-button connect-wallet-button">
+        Connect to Wallet
+      </button>
   );
-}
+
+  return (
+      <div className="App">
+        <div className="container">
+          <div className="header-container">
+            <p className="header gradient-text">My NFT Collection</p>
+            <p className="sub-text">
+              Each unique. Each beautiful. Discover your NFT today.
+            </p>
+            {renderNotConnectedContainer()}
+          </div>
+          <div className="footer-container">
+            <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+            <a
+                className="footer-text"
+                href={TWITTER_LINK}
+                target="_blank"
+                rel="noreferrer"
+            >{`built on @${TWITTER_HANDLE}`}</a>
+          </div>
+        </div>
+      </div>
+  );
+};
 
 export default App;
